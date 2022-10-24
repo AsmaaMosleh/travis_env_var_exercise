cafa2ca9c3ef79b9878938448996e78bb37c29b7
AsmaaMosleh <asmaa.mosleh2014@gmail.com>
2022-10-24 08:54:26 +0200

    add Environment Varaiable

--- server.js
+++ server.js
@@ -4,10 +4,13 @@ function sleep(ms) {
 }
 
 async function main() {
-  while(true) {
-    console.log("Microservices rock!");
-    await sleep(5000);
-  }
+  // while(true) {
+  console.log("Microservices rock!");
+  const favoriteFood = process.env.FAVORITE_FOOD;
+  console.log(`My favorite food is ${favoriteFood}`);
+
+  // await sleep(5000);
+  // }
 }
 
 main();
